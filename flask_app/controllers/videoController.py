ca9c83b89ef43e2f92b1dea0fa3fccb63eb8a9f3
Jangbahadar Singh <jsingh65@student.sierracollege.edu>
2024-08-19 17:29:34 -0700

    test

--- flask_app/controllers/videoController.py
+++ flask_app/controllers/videoController.py
@@ -7,5 +7,4 @@ from flask_app.models.videos import Video
 
 @app.route('/showvid')
 def showVid():
-    return render_template('showvid.html', videos=Video.get_all())
-
+    return render_template('showvid.html', vi
